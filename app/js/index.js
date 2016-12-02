fd183ecbce18916e3d0ee7cdf4e6e1aa298964e1
13835760532 <1125031962@qq.com>
2016-12-03 00:38:48 +0800

    123

---
+++ app/js/index.js
@@ -0,0 +1,15 @@
+$(function () {
+    function resize(originSize,type) {
+        type=type||"x";
+        if(type=="x"){
+            var widths=document.documentElement.clientWidth;
+            var scale=widths/originSize*100+"px";
+        }else if(type="y"){
+            var heights=document.documentElement.clientHeight;
+            var scale=heights/originSize*100+"px";
+
+        }
+        document.getElementsByTagName("html")[0].style.fontSize=scale;
+    }
+    resize(1334,"y")
+})
